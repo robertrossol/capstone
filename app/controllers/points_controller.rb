d1ea2f9767cba875806db1b5ec91979b15cee88d
Robert Rossol <shyguyrbr1@gmail.com>
2017-05-07 16:28:32 -0500

    adds nul check for imported files with no date from last 24 hours

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -13,27 +13,31 @@ class PointsController < ApplicationController
           p line  
           x=line.split(';') 
           time = ((x[0] + " " + x[1]).to_time)
-          if @user.entries.length==0 || @user.entries[-1].created_at < Time.now-24.hours 
-            entry=Entry.new(bg: x[2], user_id: current_user.id, created_at: time)
-            if time+24.hours >= Time.now
-              entry.save 
+          if @user.entries.length==0 || @user.entries[-1].created_at < Time.now-24.hours
+            if time+24.hours >= Time.now 
+              Entry.create(bg: x[2], user_id: current_user.id, created_at: time)
             end 
           end
         end 
       end 
       @bgs = @user.entries.where(created_at: (Time.now - 24.hours)..Time.now)
-      @daychart = {}
-      @daychart[:low]=@bgs.where("bg <= #{@user.blood_sugar_lower}").count
-      @daychart[:med]=@bgs.where("bg > #{@user.blood_sugar_lower} and bg < #{@user.blood_sugar_upper}").count
-      @daychart[:high]=@bgs.where("bg >= #{@user.blood_sugar_upper}").count
-      point = Point.new(
-        value: (@daychart[:med].to_f/@bgs.count)*10,
-        user_id: current_user.id
-        )
-      if @user.points.length == 0 || @user.points[-1].created_at < Time.now-24.hours
-        point.save
-        flash[:success] = 'Points Added Successfully'
-        redirect_to "/users/#{current_user.id}"
+      if @bgs.length != 0 
+        @daychart = {}
+        @daychart[:low]=@bgs.where("bg <= #{@user.blood_sugar_lower}").count
+        @daychart[:med]=@bgs.where("bg > #{@user.blood_sugar_lower} and bg < #{@user.blood_sugar_upper}").count
+        @daychart[:high]=@bgs.where("bg >= #{@user.blood_sugar_upper}").count
+        point = Point.new(
+          value: (@daychart[:med].to_f/@bgs.count)*10,
+          user_id: current_user.id
+          )
+        if @user.points.length == 0 || @user.points[-1].created_at < Time.now-24.hours
+          point.save
+          flash[:success] = 'Points Added Successfully'
+          redirect_to "/users/#{current_user.id}"
+        else
+          flash[:warning] = 'Points Already Added Today or File Not Selected'
+          redirect_to "/users/#{current_user.id}"
+        end
       else
         flash[:warning] = 'Points Already Added Today or File Not Selected'
         redirect_to "/users/#{current_user.id}"
